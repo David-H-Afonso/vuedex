2a7bde2b1f8fea263485ed3c65f9de851ef54ebd
David <davidhormigafonso@gmail.com>
2021-07-05 16:54:50 +0100

    Add files via upload

---
+++ src/components/PokeFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <p>
+      Created by David Hormiga Afonso &copy; 2021 - Contacto
+      <a target="_blank" href="https://github.com/David-H-Afonso">GitHub / </a
+      ><a target="_blank" href="https://www.linkedin.com/in/david-hormiga-afonso/">LinkedIn / </a
+      ><a target="_blank" href="https://twitter.com/RikkuESP">Twitter</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PokeFooter",
+};
+</script>
+
+<style scoped>
+* {
+  padding: 1px;
+  padding-left: 0px;
+}
+footer {
+  margin-top: 15px;
+}
+footer p {
+  font-size: 20px;
+}
+footer a {
+  font-size: 15px;
+  text-decoration: none;
+  color: #ffffff;
+}
+footer a:hover {
+  color: #0e8dbc;
+}
+</style>
---
+++ src/components/PokeHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="pokemon-main-title">
+      <a href="https://github.com/David-H-Afonso"
+        ><img
+          class="pokemon-main-title-image"
+          src="https://i.imgur.com/scrcyJ3.png"
+          alt="Vuedex"
+        />
+        <p>Vue<span id="another_color">Dex</span></p></a
+      >
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'PokeHeader',
+}
+</script>
+
+
+<style scoped>
+    @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+
+    .pokemon-main-title {
+      padding-top: 15px;
+      display: flex;
+      justify-content: center;
+    }
+    .pokemon-main-title-image {
+      width: 130px;
+      height: 100px;
+      margin-left: 30px;
+    }
+    .pokemon-main-title a {
+      font-family: "Press Start 2P", cursive;
+      text-decoration: none;
+      color: #41b883;
+    }
+    .pokemon-main-title a #another_color {
+      color: #34495e;
+    }
+</style>
